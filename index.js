14bfe4017abb6c779e7af6d40e35bc33c6860e5c
adam fadly <adamfadly11@gmail.com>
2018-10-17 15:02:06 +0700

    level 1 - level 3 has been solved

--- index.js
+++ index.js
@@ -2,32 +2,57 @@ let num1    = 10;
 let num2    = 099;
 const num3  = 0.02;
 
-//-- level 1 Calculation (DOM) --//
-let angka = num1 + num2
-let angka2 = num1 - num2
-let angka3 = num2 / num1
-let angka4 = num2 * num1
+//-- level 1 Calculation --//
+console.log(num1 + num2);
+console.log(num1 - num3);
+console.log(num1 * num3);
+console.log(num1 * num3 + num2);
+console.log(num2 / num3);
+console.log(num2 % num3);
 
-let tampilkan = document.getElementById("tampilkan")
-let tampilkan2 = document.getElementById("tampilkan2")
-let tampilkan3 = document.getElementById("tampilkan3")
-let tampilkan4 = document.getElementById("tampilkan4")
-tampilkan.innerHTML += angka
-tampilkan2.innerHTML += angka2
-tampilkan3.innerHTML += angka3
-tampilkan4.innerHTML += angka4
-// -- end -- //
 
-// -- Level 2 -- //
- let string = "this is string";
- let booleanTrue = true;
- let nummber= -8;
- 
-let show = string + " " + booleanTrue
-let show1= string + " " + number
+// -- Level 2 --//
+let string  = "this is string";
+let Boolean = true;
+let number  = -2
 
-let showing = document.getElementById("show")
-let showing1 = document.getElementById("show1")
+console.log(string + " " + Boolean);
+console.log(number - string);
+console.log(string + " " + number)
 
-showing.innerHTML += show
-showing1.innerHTML +=show1
+// -- Level 3 Array -- //
+let FirstName   = ["adam","gibran","haekal","wippy"];
+let LastName    = ["fadhil","Muhsin","Iqbal","Jemika","Albert"];
+let Age         = [25,28,34,29,35,21,15,31];
+
+FirstName.pop();                    // pop(): Remove an item from the end of an array.
+console.log(FirstName);
+Age.shift ();                       // shift(): Remove an item from the beginning of an array
+console.log(Age);
+LastName.push("william","haidar");  // push(): Add items to the end of an array.
+console.log(LastName)
+FirstName.unshift("billy","rossa"); // unshift(): Add items to the beginning of an array
+console.log(FirstName);
+
+for (i=0;i<Age.length;i++){
+    console.log(Age[i])
+}
+// -- Requirments -- //
+let longString = "This is a very long string which needs " +
+                 "to wrap across multiple lines because " +
+                 "otherwise my code is unreadable.";
+
+function sortThings(a, b) {
+  a = a.toLowerCase(longString);
+  b = b.toLowerCase(longString);
+
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  } else if (a === b) {
+    return 0;
+  }
+}
+
+console.log (longThings);
